98123f4c4fa96b5bfc42e052d6b15209dec2d90a
BadBoy <khamidjonashuraliev033@gmail.com>
2023-08-23 10:27:29 +0900

    for loop

--- Javascript/Js.js
+++ Javascript/Js.js
@@ -1,4 +1,70 @@
-" use strict";
-const number = 10;
-const clientN
-console.log(number);
+// " use strict";
+// // const number = 10;
+
+// // console.log(number);
+
+// let maxSpeed = 15
+// // console.log(maxSpeed);                      
+// // console.log('4'+4);
+
+
+// // alert('hello world')
+// confirm("sizi yoshiz 30mi")
+
+
+// for loop
+// first way
+// let answers = []
+// let questions = [
+//     "What is your name",
+//     "What is your phone",
+//     "How old are you"
+// ]
+
+
+// for (let i = 0; i < questions.length; i++) {
+// answers[i] = prompt (questions[i])
+// }
+//     console.log(answers);
+
+// second way
+// let answers = []
+// let questions = [
+//     "What is your name",
+//     "What is your phone",
+//     "How old are you"
+// ]
+// let i = 0; 
+// do { answers[i] = prompt (questions[i])
+//     console.log(answers);
+//     i++
+// } while (i < questions.length )
+
+
+// third way
+// let answers = []
+// let questions = [
+//     "What is your name",
+//     "What is your phone",
+//     "How old are you"
+// ]
+// let i = 0 
+// while(i < questions.length ) {
+//     console.log(answers);
+//     i++
+// } 
+// answers[i] = prompt (questions[i]);
+
+
+
+
+// let answers = []
+// let questions = [
+//     "What is your name",
+//     "What is your phone",
+//     "How old are you"
+// ]
+// for (let i = 0; i < questions.length; i++) {
+//     answers[i] = prompt (questions[i])
+// }
+// console.log(answers);
