97fcb568e9bf170e4cacd6509fe175adb4147dd8
BadBoy <khamidjonashuraliev033@gmail.com>
2023-08-28 23:39:22 +0900

    feat : edit code

--- Javascript/Js.js
+++ Javascript/Js.js
@@ -273,7 +273,39 @@
 // }
 
 
+// Amaliyot uyga vazifa 2
+const numberOfSeries = prompt("Nechta serial ko'rdingiz?");
+ 
+const seriesDB = {
+    count: numberOfSeries,
+    series: {},
+    actors: {},
+    genres: [],
+    private: false,
+};
 
+for (let i = 0; i < 2; i++){
+    const a = prompt("Oxirgi ko'rgan serialingiz?"),
+b = prompt("Nechchi baho berasiz?");
+
+if (a != null && b != null && a  != "" && b != "") {
+    seriesDB.series[a] = b;
+    console.log('done');
+} else {
+    console.log('error');
+    i--;
+ }
+}
+if (seriesDB.count < 5) {
+    console.log("Kam serial ko'ribsiz");
+} else if (seriesDB.count >= 5 && seriesDB.count < 10) {
+    console.log("Siz classik tomoshabin ekansiz");
+} else if (seriesDB.count >= 10) {
+    console.log("Siz serialchi zvezda ekansiz");
+} else {
+console.log("Error");
+}
+console.log(seriesDB);
 
 
 
